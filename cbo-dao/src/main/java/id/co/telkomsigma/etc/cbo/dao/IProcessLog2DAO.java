307c43793a5f2554241934e2f75de4e6d6e36a30
KNIGHTMASTER <fauzi.knightmaster.achmad@gmail.com>
2017-10-16 13:10:38 +0700

    Add support HATEOAS

---
+++ cbo-dao/src/main/java/id/co/telkomsigma/etc/cbo/dao/IProcessLog2DAO.java
@@ -0,0 +1,9 @@
+package id.co.telkomsigma.etc.cbo.dao;
+
+/**
+ * Created on 10/16/17.
+ *
+ * @author <a href="mailto:fauzi.knightmaster.achmad@gmail.com">Achmad Fauzi</a>
+ */
+public interface IProcessLog2DAO {
+}
---
+++ cbo-data/src/main/java/id/co/telkomsigma/etc/cbo/data/model/ReferenceList.java
@@ -0,0 +1,9 @@
+package id.co.telkomsigma.etc.cbo.data.model;
+
+/**
+ * Created on 10/16/17.
+ *
+ * @author <a href="mailto:fauzi.knightmaster.achmad@gmail.com">Achmad Fauzi</a>
+ */
+public class ReferenceList {
+}
